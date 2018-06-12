71e7c1fed0b25e34b96859413d8c1b50c122da44
Michael Lomnitz <mlomnitz@Michaels-MacBook-Pro.local>
2018-06-11 17:49:43 -0700

    Add ops for general utilities related to preprocessing

---
+++ ops/ops.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Preprocessing images helper for face-compression GAN
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from PIL import Image, ImageOps
+import numpy as np
+
+
+def img_preprocess( image_file, re_sample, greyscale = True):
+    """
+    Method used to do image pre-processing for GAN network. 
+    Default sets to grayscale, fits to square canvas of size equal to the largest side
+    and finally down (or upsamples) to the value given.
+    """
+    image = Image.open(image_file)
+    if( greyscale ):
+        img = image.convert('L')
+    side = max(image.size)    
+    img = fit_to_canvas(img, side, side)
+    return img.resize(re_sample )
+    
+def fit_to_canvas(image, desired_w, desired_h, method=Image.ANTIALIAS):
+    """
+    resize 'image' to 'desired_size' keeping the aspect ratio 
+    and place it in center of white 'max_size' image 
+    """
+    old_size = image.size
+    ratio_w = float(desired_w)/old_size[1]
+    ratio_h = float(desired_h)/old_size[0]
+    #
+    delta_w = desired_w - old_size[0]
+    delta_h = desired_h - old_size[1]
+    padding = (delta_w//2, delta_h//2, delta_w-(delta_w//2), delta_h-(delta_h//2))
+    new_im = ImageOps.expand(image, padding)
+    return new_im
