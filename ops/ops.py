611cd6c1e29db0d993cf723262db5103857b71ce
Michael Lomnitz <mllomnitz@gmail.com>
2018-06-17 13:23:40 -0700

    Move restore to its own file

--- ops/ops.py
+++ ops/ops.py
@@ -4,7 +4,6 @@ import os
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import tensorflow as tf
 from PIL import Image, ImageOps
 import numpy as np
 
@@ -42,24 +41,3 @@ def fit_to_canvas(image, desired_w, desired_h, method=Image.ANTIALIAS):
     padding = (delta_w//2, delta_h//2, delta_w-(delta_w//2), delta_h-(delta_h//2))
     new_im = ImageOps.expand(image, padding)
     return new_im
-
-def restore_chkp(ckpt_file, output_ckpt):
-    """
-    Restore checkpoint from available index, meta and weights files. Assumes all three are in the same directory
-    Args:
-    - ckpt_file: Path to tensorflow files with the included basename, i.e. gan-train_epoch15.ckpt-15.
-    Output:
-    Produces a copy of the three tensorflow files together with checkpoint
-    """
-    termination = ['.meta', '.index', '.data-00000-of-00001']
-    files_ok = True
-    
-    for term in termination:
-        if not os.path.isfile(ckpt_file+term):
-            files_ok = False
-    assert files_ok, 'Files are missing, can not restore'
-    
-    with tf.Session() as sess:
-        saver = tf.train.import_meta_graph(ckpt_file+'.meta')
-        saver.restore(sess, ckpt_file)
-        saver.save(sess,output_ckpt)
